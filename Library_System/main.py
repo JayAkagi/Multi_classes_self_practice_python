66d5959174c95c87a70cf3894af51a8bf81d8f2d
Chris Bautista <b019782k@gmail.com>
2025-10-25 07:17:32 +0100

    Library

--- Library_System/main.py
+++ Library_System/main.py
@@ -4,8 +4,7 @@ from book import *
 book1 = Book("Title1", "Author1")
 book2 = Book("Title2", "Author2")
 book3 = Book("Title3", "Author3")
-book4 = Book("Title4", "Author4")
-book5 = "Title5, Author5"
+book4 = "Title5, Author5"
 
 city_library = Library()
 
@@ -13,7 +12,6 @@ city_library.add_book(book1)
 city_library.add_book(book2)
 city_library.add_book(book3)
 city_library.add_book(book4)
-city_library.add_book(book5)
 
 for book in city_library.get_all_books():
     print(book.description())
